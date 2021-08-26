b4706e38af5d5883876f4b6178b0da29fa678f65
excela01 <excela01@gmail.com>
2021-08-26 21:04:02 +1245

    animations added

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+    
+    /* FOR STICKY NAVI*/
     $('.js--section-features').waypoint(function(direction) {
         
         if (direction == "down") {
@@ -11,6 +13,98 @@ $(document).ready(function() {
         offset: '60px;'
     });
 
+
+    
+    /* SCROLL ON BUTTON*/
+    
+    
+    $('.js--scroll-to-plans').click(function() {
+        $('html,body').animate({scrollTop: $('.js--section-plans').offset().top},1000)
+        
+        
+    });
+    
+    $('.js--scroll-to-start').click(function() {
+        $('html,body').animate({scrollTop: $('.js--section-features').offset().top},1000)
+        
+        
+    });
+    
+    /*NAVIGATION SCROLL*/
+    
+    
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+              if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                  $('html,body').animate({
+                   scrollTop: target.offset().top
+                  }, 1000);
+                  return false;
+                 }
+                }
+              });      
+            });
+        
+    
+    
+    /* ANIMATIONS ON SCROLL*/
+    
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+        
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated backInLeft');
+    },  {
+        offset: '50%'
+        
+    });
+        
+        
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated backInUp');
+    },  {
+        offset: '50%'
+        
+    });
+        
+        
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated fadeInUp');
+    },  {
+        offset: '50%'
+        
+    });
+        
+        
+        
+    $('.js--wp-5').waypoint(function(direction) {
+        $('.js--wp-5').addClass('animated backInDown');
+    },  {
+        offset: '50%'
+        
+    });
+        
+        
+        
+    $('.js--wp-6').waypoint(function(direction) {
+        $('.js--wp-6').addClass('animated heartBeat');
+    },  {
+        offset: '50%'
+        
+    });
+        
+        
+        
+        
+    });
+
+    
     
     
     
@@ -20,5 +114,4 @@ $(document).ready(function() {
       offset: '25%'
     })  */
     
-});
 
