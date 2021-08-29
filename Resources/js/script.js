f291ea5a1f198d3e670cfe6aa8d952d5c4196431
excela01 <excela01@gmail.com>
2021-08-29 14:16:45 +1245

    responsive nav bar for mobile

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -107,20 +107,33 @@ $(document).ready(function() {
         offset: '50%'
         
     });
+    
+    
+    
+    /*MOBILE NAV*/
+    
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
         
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        
+        if(icon.hasClass('ion-navicon-round')) {
+          icon.addClass('ion-close-round');  
+          icon.removeClass('ion-navicon-round');  
+        } else {
+            
+          icon.addClass('ion-navicon-round');  
+          icon.removeClass('ion-close-round'); 
+       
+        }
+        
+    });
         
         
         
 });
 
-    
-    
-    
-    
-        /*   var waypoints = $('#handler-first').waypoint(function(direction) {
-      notify(this.element.id + ' hit 25% from top of window') 
-    }, {
-      offset: '25%'
-    })  */
-    
+  
 
